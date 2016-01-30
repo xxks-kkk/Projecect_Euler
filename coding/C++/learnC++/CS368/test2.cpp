b9edbea57636b5c04288dc2456cb98d55a24d0bb
Zeyuan Hu <ferrishu3886@gmail.com>
2016-01-29 22:45:07 -0600

    Finish test2

--- coding/C++/learnC++/CS368/test2.cpp
+++ coding/C++/learnC++/CS368/test2.cpp
@@ -2,6 +2,17 @@
 
 using namespace std;
 
+bool arrayEq(int A[], int B[], int len);
+
+int main()
+{
+  int A[3] = {0,1,2};
+  int B[3] = {0,1,2};
+  int C[3] = {1,0,2};
+  cout << arrayEq(A, B, 3) << endl;
+  cout << arrayEq(A, C, 3) << endl;
+}
+
 bool arrayEq(int A[], int B[], int len)
 {
   for (int i = 0; i < len; i++)
